50d67aea26f4f6d0b755e98861d35ff69ac4ddf0
Ajeesh <ajeeshrs79@gmail.com>
2024-06-02 19:30:42 +0530

    socket io initial setup

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,23 @@
+import { useEffect } from 'react';
 import AllChats from '../components/AllChats';
 import ChatView from '../components/ChatView';
 import NavBar from '../components/NavBar';
 import "../styles/scrollBar.css"
-function HomePage() {
+import { io } from "socket.io-client";
 
+function HomePage() {
+  const socket = io("localhost:8001")
+
+  useEffect(() => {
+    socket.io.on("open", () => {
+      console.log("connection succeded")
+    })
+
+
+    return () => {
+      socket.disconnect()
+    }
+  }, [])
   return (
     <div className="flex sm:flex-row flex-col-reverse sm:justify-start justify-between w-[100%] h-[100vh] bg-[#fff]">
       <NavBar />
